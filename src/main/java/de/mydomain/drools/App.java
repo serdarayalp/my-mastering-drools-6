7ae287259550c639ccdb4d8ec405ba68abec4667
serdarayalp <serdarayalp@googlemail.com>
2022-01-30 17:21:01 +0100

    Kleine Changes

--- src/main/java/de/mydomain/drools/App.java
+++ src/main/java/de/mydomain/drools/App.java
@@ -6,30 +6,31 @@ import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 
 public class App {
+
     public static void main(String[] args) {
 
         System.out.println("Bootstrapping the Rule Engine...");
 
         System.out.println("Bootstrapping a Rule Engine Session...");
-        KieServices ks = KieServices.Factory.get();
-        KieContainer kContainer = ks.getKieClasspathContainer();
-        KieSession kSession = kContainer.newKieSession();
+        KieServices kieServices = KieServices.Factory.get();
+        KieContainer kieContainer = kieServices.getKieClasspathContainer();
+        KieSession kieSession = kieContainer.newKieSession();
 
         Item item = new Item("A", 123.0, 234.0);
         System.out.println("Item Category: " + item.getCategory());
 
-        kSession.insert(item);
+        kieSession.insert(item);
 
         /*
         Item item2 = new Item("B", 250.0, 234.0);
-        kSession.insert(item2);
+        kieSession.insert(item2);
         Item item3 = new Item("C", 300.0, 234.0);
-        kSession.insert(item3);
+        kieSession.insert(item3);
         Item item4 = new Item("D", 100.0, 234.0);
-        kSession.insert(item4);
+        kieSession.insert(item4);
         */
 
-        int fired = kSession.fireAllRules();
+        int fired = kieSession.fireAllRules();
         System.out.println("Number of Rules executed = " + fired);
 
         System.out.println("Item Category: " + item.getCategory());
